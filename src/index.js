913e755b41cdad5944d2f7ec63ea1836485552aa
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-07-25 18:18:17 +1200

    Add initial pre-packaged results.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
+
 exports.mapcore_pre_packaged_results_module = function()  {
 
+
+  this.get_results = () => {
+    let results = [
+    /*
+      {
+        "BlackfynnID": "",
+        "Description": "",
+        "Example Image": "",
+        "Dataset Title": "",
+      },
+    */
+      {
+        "BlackfynnID": "",
+        "Dataset Title": "Human Flatmap",
+        "Description": "This dataset holds a description of a flatmap representation of the major human organs.",
+        "Example Image": "", "Species": "NCBITaxon:9606"
+      },
+      {
+        "BlackfynnID": "",
+        "Dataset Title": "Rat Flatmap",
+        "Description": "This dataset holds a description of a flatmap representation of the major rat organs.",
+        "Example Image": "", "Species": "NCBITaxon:XXXX"
+      },
+    ]
+
+    return results
+  }
 
   const initialise = () => {
     console.log("Initialise pre-packaged results.");
