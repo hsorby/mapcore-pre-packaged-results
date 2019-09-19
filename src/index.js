f8ff9b9aa278387cc663d70860748e4cb6ab8a3c
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-09-20 10:27:25 +1200

    Merge the result for the 'stellate' search term into the pre-packaged result.

--- src/index.js
+++ src/index.js
@@ -35,7 +35,18 @@ exports.mapcore_pre_packaged_results_module = function()  {
         "Dataset Title": "Cell Body Segmentation and Electrophysiology Data: Stellate Ganglion",
         "Description": "Mouse stellate ganglion neuronal cell shape data from the Shivkumar/Tompkins group are displayed in a 3D stellate scaffold. 15 neurons, including their cell bodies, axons and dendrites, are displayed on five cross-sections of the stellate with the high resolution image displayed as a texture map. Electrophysiological data from these cells can be visualized by clicking on the cell. Note that the scaffold in this example is a geometrically simple shape designed to register the images in the appropriate anatomical location. The shape of the scaffold will be improved once more data are available to define the stellate boundaries.",
         "Example Image": "",
-        "Scaffold": {'uri': 'https://mapcore-bucket1.s3-us-west-2.amazonaws.com/ISAN/scaffold/stellate/stellate_metadata.json', 'species': 'Mouse', 'organ': 'stellate', 'annotation': 'UBERON:0002440'}
+        "Scaffold": {
+          "uri": "https://mapcore-bucket1.s3-us-west-2.amazonaws.com/ISAN/scaffold/stellate/stellate_metadata.json",
+          'species': 'Mouse',
+          'organ': 'nerve',
+          'annotation': 'UBERON:0002440'
+        },
+        "DataViewer": {
+          "uri": "https://mapcore-bucket1.s3-us-west-2.amazonaws.com/ISAN/csv-data/use-case-2/Sample_1_18907001_channel_1.csv",
+          'species': 'Mouse',
+          'organ': 'nerve',
+          'annotation': 'UBERON:0002440'
+        }
       },
       {
         "BlackfynnID": "",
